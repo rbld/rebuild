14077672ff888934b059ec7c4c5a43846733fe6c
Dmitry Fleytman <dmitry@daynix.com>
2016-06-14 11:08:08 +0300

    tests: Add Hello World test
    Signed-off-by: Dmitry Fleytman <dmitry@daynix.com>

---
+++ cli/tests/hello/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#ifndef ENV_NAME
+#  define ENV_NAME "host"
+#endif
+
+int main(int argc, char* argv)
+{
+    printf("Hello World from Rebuild environment " ENV_NAME "\n");
+    return 0;
+}
