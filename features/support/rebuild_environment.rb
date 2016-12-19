5a3955850cbc516aba30b4b089ac57d60d969106
Dmitry Fleytman <dmitry@daynix.com>
2016-12-19 12:38:13 +0200

    tests: Always use the same rbld executable both explicitly and implicitly
    Signed-off-by: Dmitry Fleytman <dmitry@daynix.com>

--- features/support/rebuild_environment.rb
+++ features/support/rebuild_environment.rb
@@ -1,58 +1,66 @@
 class RebuildEnvMgr
-  def self.run_command(command)
-    output = %x(#{command} 2>&1)
-    fail "Failed to run \"#{command}\": #{output}" unless $?.success?
+  def self.run_rbld_command(command)
+    if ENV['TEST_WORKING_COPY'] == '1'
+      rbld = File.expand_path('../../../cli/bin/rbld', __FILE__)
+    else
+      rbld = 'rbld'
+    end
+
+    cmdline = "#{rbld} #{command}"
+
+    output = %x(#{cmdline} 2>&1)
+    fail "Failed to run \"#{cmdline}\": #{output}" unless $?.success?
     return output
   end
 
-  private_class_method :run_command
+  private_class_method :run_rbld_command
 
   def self.list
-    run_command("rbld list")
+    run_rbld_command("list")
   end
 
   def self.checkout(name)
-    run_command("rbld checkout #{name}")
+    run_rbld_command("checkout #{name}")
   end
 
   def self.rm(name)
-    run_command("rbld rm #{name}")
+    run_rbld_command("rm #{name}")
   end
 
   def self.publish(name)
-    run_command("rbld publish #{name}")
+    run_rbld_command("publish #{name}")
   end
 
   def self.status
-    run_command("rbld status")
+    run_rbld_command("status")
   end
 
   def self.search
-    run_command("rbld search")
+    run_rbld_command("search")
   end
 
   def self.modify(name)
-    run_command("rbld modify #{name} -- echo Modifying...")
+    run_rbld_command("modify #{name} -- echo Modifying...")
   end
 
   def self.commit(name, tag)
-    run_command("rbld commit --tag #{tag} #{name}")
+    run_rbld_command("commit --tag #{tag} #{name}")
   end
 
   def self.create(name, base)
-    run_command("rbld create --base #{base} #{name}")
+    run_rbld_command("create --base #{base} #{name}")
   end
 
   def self.run(name, cmd)
-    run_command("rbld run #{name} -- #{cmd}")
+    run_rbld_command("run #{name} -- #{cmd}")
   end
 
   def self.save_to(name, file)
-    run_command("rbld save #{name} #{file}")
+    run_rbld_command("save #{name} #{file}")
   end
 
   def self.load_from(file)
-    run_command("rbld load #{file}")
+    run_rbld_command("load #{file}")
   end
 end
 
