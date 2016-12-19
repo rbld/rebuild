0b8b7fb5c0fc06164c92294523bae7b0516d0eb5
Dmitry Fleytman <dmitry@daynix.com>
2016-12-19 11:42:16 +0200

    tests: Fix handling of cucumber profiles
    It is wrong to change environment variable PATH
    in cucumber.yml because in case it contains spaces
    it gets truncated after the first space.
    
    This patch implements proper modification of this
    environment variable and makes test suite functional
    on Windows.
    
    Signed-off-by: Dmitry Fleytman <dmitry@daynix.com>

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,6 +2,11 @@ Before do
   cfg_file=rebuild_conf.path_name
   cfg_file_backup=cfg_file + ".backup"
   FileUtils.cp(cfg_file, cfg_file_backup) if File.exist?(cfg_file)
+
+  if ENV['TEST_WORKING_COPY'] == '1'
+    prepend_environment_variable('PATH',
+     format("%s#{File::PATH_SEPARATOR}", File.expand_path('../../../cli/bin', __FILE__)))
+  end
 end
 
 After do
