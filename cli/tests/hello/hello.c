6a5ae45aad7943b8da905f2f1d7c894e412843ea
Yan Vugenfirer <yan@daynix.com>
2017-03-07 15:53:27 +0200

    tests: Fix main parameter of the test application
    [ci skip]
    
    Signed-off-by: Yan Vugenfirer <yan@daynix.com>

--- cli/tests/hello/hello.c
+++ cli/tests/hello/hello.c
@@ -4,7 +4,7 @@
 #  define ENV_NAME "host"
 #endif
 
-int main(int argc, char* argv)
+int main(int argc, char** argv)
 {
     printf("Hello World from Rebuild environment " ENV_NAME "\n");
     return 0;
